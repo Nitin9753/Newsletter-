7859ddf8a0c1b7e1434a3c970171c7848c06c807
BuNNY <nitin200103085@iitg.ac.in>
2023-05-23 20:57:09 +0530

    first commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
-const api_key = "794b4fbdfcecf0bb712960bf0be528e5-us14";
+const api_key = "6b47301de546b9f9fa1815b57375ea5c-us14";
 const list_id = "243a521cf5";
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/signup.html");
@@ -28,11 +28,16 @@ app.post("/", function(req, res) {
     }
     const jsonData = JSON.stringify(data);
     const url = "https://us14.api.mailchimp.com/3.0/lists/243a521cf5";
-    const options = {
+    var options = {
         method: "POST",
         auth: "Bunndi:" + api_key
     }
     const request = https.request(url, options, function(response) {
+        if (response.statusCode == 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
         response.on("data", function(data) {
             console.log(JSON.parse(data));
         })
@@ -41,6 +46,11 @@ app.post("/", function(req, res) {
     request.end();
 })
 
-app.listen(3000, function() {
+
+app.post("/failure", function(req, res) {
+    res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000, function() {
     console.log("Server started at port 3000.");
 })
